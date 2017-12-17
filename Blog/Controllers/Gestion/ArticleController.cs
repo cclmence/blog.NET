16f2e3aeb20a1ebbbfe56418ba84e89dce627d86
C-c-l <cecilia.mcszk@gmail.com>
2017-12-17 13:42:08 +0100

    initial commit

---
+++ Blog/Controllers/Gestion/ArticleController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers.Gestion
+{
+    [Authorize]
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Articles/Creer
+        public ActionResult Creer()
+        {
+            return View();
+        }
+
+        // POST: Articles/Creer
+        [HttpPost]
+        public ActionResult Creer (FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Articles/Modif/ArticleID
+        public ActionResult Modif(int id)
+        {
+            return View();
+        }
+
+        // POST: Article/Modif/ArticleID
+        [HttpPost]
+        public ActionResult Modif(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO : Add update logic here
+                return RedirectToAction("Index");
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: Article/Suppr/ArticleID
+        public ActionResult Suppr(int id)
+        {
+            return View();
+        }
+
+        // POST: Article/Suppr/ArticleID
+        [HttpPost]
+        public ActionResult Suppr(int id, FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Blog/Controllers/Salutation/SalutationController.cs
@@ -0,0 +1,30 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers.Salutation
+{
+    public class SalutationController : Controller
+    {
+        // GET: Salutation
+        public ActionResult Index()
+        {
+            Visiteur client = new Visiteur();
+            return View(client);
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Index(Visiteur visiteur)
+        {
+            Visiteur client = new Visiteur();
+            string prenom = "";
+
+            prenom = Request.Form["prenom_visiteur"];
+            client.Prenom = prenom;
+            ViewData["message"] = "Bonjour à toi, " + prenom;
+            return View("Index", client);
+        }
+    }
+}
